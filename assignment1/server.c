d814561389c85c7350070b7296f9966396f5b62a
myrmecobius <36750034+myrmecobius@users.noreply.github.com>
2021-04-02 19:01:29 -0700

    Update server.c

--- assignment1/server.c
+++ assignment1/server.c
@@ -55,15 +55,18 @@ int main(int argc, char const *argv[])
         perror("accept");
         exit(EXIT_FAILURE);
     }
+    
+    // Fork starting here
+    
     if(fork() ==0){
-    setuid(65534);
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
-    return 0;
+        setuid(65534); // set to nobody user
+        valread = read( new_socket , buffer, 1024);
+        printf("%s\n",buffer );
+        send(new_socket , hello , strlen(hello) , 0 );
+        printf("Hello message sent\n");
+        return 0;
     }
     else{
-    wait(NULL);
+        wait(NULL); // wait for child processes to finish
     }
 }
